3d30b44395a7bbd55615e2ef9337d9c76dba5f6f
DAP2106 <pachkovskiid82@gmail.com>
2023-04-05 19:45:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        int ticketPrice = 20_000;
+
+        int bonus = 20;
+
+        int bonusMiles = ticketPrice/ bonus ;
+        System.out.println(bonusMiles +   " бонусов ");
+    }
+}
