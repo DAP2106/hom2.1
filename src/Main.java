f8abcc25cabed93251e39d54a79ab7f1a01442c5
DAP2106 <pachkovskiid82@gmail.com>
2023-04-05 20:43:28 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
         int ticketPrice = 20_000;
 
         int bonus = 20;
 
-        int bonusMiles = ticketPrice/ bonus ;
-        System.out.println(bonusMiles +   " бонусов ");
+        int bonusMiles = ticketPrice / bonus;
+
+        System.out.println(bonusMiles + " бонусов ");
     }
 }
